c22a23f16b1af64935f41f33d7af493b0826ac20
Natan-Makielkowski <natanmakielkowski@gmail.com>
2026-06-29 12:32:25 +0200

    feat: initial structure

---
+++ src/main/java/org/example/hardwarepricetracker/HardwarePriceTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.example.hardwarepricetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwarePriceTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwarePriceTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hardwarepricetracker/HardwarePriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hardwarepricetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwarePriceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
