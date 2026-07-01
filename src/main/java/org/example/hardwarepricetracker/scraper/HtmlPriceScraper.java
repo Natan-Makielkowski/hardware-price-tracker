c66a9f7ca9426514bf515a3356614424170426df
Natan-Makielkowski <natanmakielkowski@gmail.com>
2026-07-01 13:02:10 +0200

    test: added initial structure of scraper

---
+++ src/main/java/org/example/hardwarepricetracker/scraper/HtmlPriceScraper.java
@@ -0,0 +1,8 @@
+package org.example.hardwarepricetracker.scraper;
+
+public class HtmlPriceScraper {
+    public Double extractPrice(String htmlContent, String selector) {
+        return 0.0;
+    }
+
+}
